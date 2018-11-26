7a48acc7c6806d8daeefa0022da03b042a839f68
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-27 01:25:03 +0800

    Component Reuse

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
         <CommentDetail
           author="Sam"
           timeAgo="Today at 4:45PM"
@@ -15,6 +22,7 @@ const App = () => {
           avatar={faker.image.avatar()}
         />
       </ApprovalCard>
+
       <ApprovalCard>
         <CommentDetail
           author="Alex"
@@ -23,6 +31,7 @@ const App = () => {
           avatar={faker.image.avatar()}
         />
       </ApprovalCard>
+
       <ApprovalCard>
         <CommentDetail
           author="Jane"
