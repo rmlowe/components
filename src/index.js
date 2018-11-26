9238b280a71532923c2fa016ece39e9e47a4fc6a
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-26 21:31:01 +0800

    Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Sam" />
+      <CommentDetail author="Alex" />
+      <CommentDetail author="Jane" />
     </div>
   );
 };
